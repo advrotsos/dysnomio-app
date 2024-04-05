bf4325c79f308744ecb1d786374bedbba9c071f9
Alexios Vrotsos <103211040+advrotsos@users.noreply.github.com>
2024-04-05 13:13:31 -0400

    new prompts

--- data/hint_prompts.py
+++ data/hint_prompts.py
@@ -1,16 +1,22 @@
 import random
 
-actors = ["Tom Cruise", "Leonardo DiCaprio", "Robert DeNiro"]
+actors = [
+    "Tom Cruise",
+    "Leonardo DiCaprio",
+    "Robert DeNiro",
+    "Paul Dano",
+    "Ethan Hawke",
+]
 
 hint_prompts = [
     "write a haiku",
     "write a brief sonnet",
     "create a fake rap verse",
     "create a fake country song verse",
-    "write a short synopsis of fake movie starring {}".format(
+    "write a short synopsis of fake movie starring {}. Give the movie a fake name, and pretend it's real.".format(
         actors[random.choice(range(len(actors)))]
     ),
-    # "describe a delicious meal prepared by a fine dining chef. Describe the food.",  # this seems to be producing odd results
     "introduce a fight between two boxers in an old-timey prose",
-    "describe a fake half-time show performance at the Super Bowl",
+    "write a short poem",
+    "make a pitch to the judges on Shark Tank",
 ]
